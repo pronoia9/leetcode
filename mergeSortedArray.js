d3831227a823fdb9098f4bf9970781bf801f6f95
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-04 08:42:37 +0300

    problem #88

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -4,8 +4,16 @@
 // The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 // https://leetcode.com/problems/merge-sorted-array/
 //
-const merge = (nums1, m, nums2, n) => {
+// Runtime:      96 ms,   faster than 25.13%   |   68 ms,   faster than 76.46%   |   108 ms,  faster than 12.95%   |   79 ms,   faster than 51.54%
+// Memory Usage: 42.3 MB, less than   28.54%   |   42.3 MB, less than   28.54%   |   42.4 MB, less than   28.54%   |   42.2 MB, less than   55.42%
+
+export default function merge (nums1, m, nums2, n) {
+  for (let i = m; i < nums1.length; i++) {
+    nums1[i] = nums2[i - m];
+  }
+  nums1.sort((a, b) => a - b);
 };
+
 // ************************************************************************************************************************ //
 
-console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+// merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
