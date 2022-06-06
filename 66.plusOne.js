20ed51de8d53ee27328f32fb30b5d6ad2663ea33
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 06:43:51 +0300

    done (2 ways)

--- 66.plusOne.js
+++ 66.plusOne.js
@@ -4,11 +4,28 @@
 // Increment the large integer by one and return the resulting array of digits.
 // https://leetcode.com/problems/plus-one/
 //
+// Runtime:       95 ms,   faster than 23.96%   |   76 ms,   faster than 54.78%   |   108 ms,  faster than 10.37%
+// Memory Usage:  42.6 MB, less than    5.20%   |   42.3 MB,  less than  18.13%   |   42.3 MB, less than   32.87%
+{
+  const plusOne = (digits) => {
+    return Array.from((BigInt(digits.join('')) + BigInt(1)).toString(), (n) => BigInt(n));
+  };
+}
 //
-//
-const plusOne = (digits) => {};
+// Runtime:      65 ms,   faster than 81.52%
+// Memory Usage: 42.3 MB, less than   32.87%
+{
+  const plusOne = (digits) => {
+    return Array.from(
+      `${digits.reduce((prev, curr, idx) => (prev += BigInt(curr.toString() + '0'.repeat(digits.length - idx - 1))), BigInt(0)) +
+        BigInt(1)}`,
+      (n) => parseInt(n)
+    );
+  };
+}
 // ************************************************************************************************************************ //
 
-console.log(plusOne([1, 2, 3]));
-console.log(plusOne([4, 3, 2, 1]));
-console.log(plusOne([9]));
+// console.log(plusOne([1, 2, 3]));
+// console.log(plusOne([4, 3, 2, 1]));
+// console.log(plusOne([9]));
+// console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
