022632c02869df45696bbf3b8f65354c4cc20749
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-12 07:43:09 +0300

    problem #35

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -3,12 +3,31 @@
 // You must write an algorithm with O(log n) runtime complexity.
 // https://leetcode.com/problems/search-insert-position/
 //
-var searchInsert = function (nums, target) {
-  return indexOf();
-};
+// Runtime:      99 ms,   faster than 20.23%
+// Memory Usage: 42.3 MB, less than   34.56%
+{
+  const searchInsert = (nums, target) => {
+    let left = 0,
+      right = nums.length - 1,
+      middle;
+
+    if (target > nums[right]) return right + 1;
+    else if (target < nums[left]) return left;
+    else
+      while (left < right) {
+        middle = Math.floor((left + right) / 2);
+
+        if (nums[middle] === target) return middle;
+        else if (target > nums[middle]) left++;
+        else if (target < nums[middle]) right--;
+      }
+
+    return left;
+  };
+}
+//
 // ************************************************************************************************************************ //
-console.log(searchInsert([1, 3, 5, 6], 5));
-console.log(searchInsert([1, 3, 5, 6], 2));
+// console.log(searchInsert([1, 3, 5, 6], 5));
+// console.log(searchInsert([1, 3, 5, 6], 2));
 console.log(searchInsert([1, 3, 5, 6], 7));
 // console.log(searchInsert());
-
