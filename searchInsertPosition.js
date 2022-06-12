34e39c70ab082bfa6ce17bb632e5f7cc11fa4468
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-12 06:28:15 +0300

    problem #35 init

---
+++ searchInsertPosition.js
@@ -0,0 +1,14 @@
+// ********************************************   35. Search Insert Position   ******************************************** //
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+// https://leetcode.com/problems/search-insert-position/
+//
+var searchInsert = function (nums, target) {
+  return indexOf();
+};
+// ************************************************************************************************************************ //
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
+// console.log(searchInsert());
+
