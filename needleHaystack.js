61f0da90cd311ac0745e811930d2ed54ec09157e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-17 13:01:00 +0300

    problem #28

--- needleHaystack.js
+++ needleHaystack.js
@@ -6,7 +6,7 @@
 // For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
 // https://leetcode.com/problems/implement-strstr/
 //
-// Runtime:       67 ms,    faster than 76.78% 
+// Runtime:       67 ms,    faster than 76.78%
 // Memory Usage:  41.6 MB,  less than   90.89%
 {
   const strStr = (haystack, needle) => {
@@ -21,13 +21,13 @@
 //
 // Runtime:       58 ms,    faster than 94.83 %
 // Memory Usage:  41.5 MB,  less than   94.65%
-export default function strStr(haystack, needle) {
+const strStr = (haystack, needle) => {
   for (let i = 0; i < haystack.length; i++) {
     if (haystack[i] === needle[0] && haystack.slice(i, i + needle.length) == needle) return i;
   }
   return -1;
-}
+};
 // ************************************************************************************************************************ //
 
-// console.log(strStr('hello', 'll')); //  2
-// console.log(strStr('aaaaa', 'bba')); // -1
+console.log(strStr('hello', 'll')); //  2
+console.log(strStr('aaaaa', 'bba')); // -1
