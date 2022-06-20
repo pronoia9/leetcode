d4e099209744adbd5d4fa8adb1ac62092402fa96
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-20 16:57:21 +0300

    problem #326 init

---
+++ powerOfThree.js
@@ -0,0 +1,24 @@
+// ***********************************************   326. Power of Three   ************************************************ //
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+// https://leetcode.com/problems/power-of-three/
+//
+const isPowerOfThree = (n) => {
+};
+// ************************************************************************************************************************ //
+
+console.log(isPowerOfThree(27));
+console.log(isPowerOfThree(0));
+console.log(isPowerOfThree(9));
+
+// Example 1:
+// Input: n = 27
+// Output: true
+
+// Example 2:
+// Input: n = 0
+// Output: false
+
+// Example 3:
+// Input: n = 9
+// Output: true
