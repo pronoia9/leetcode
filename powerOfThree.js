bc56d4cd4590a5bc7e5909de53c568e24e1f4847
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-20 17:01:55 +0300

    problem #326

--- powerOfThree.js
+++ powerOfThree.js
@@ -3,7 +3,15 @@
 // An integer n is a power of three, if there exists an integer x such that n == 3x.
 // https://leetcode.com/problems/power-of-three/
 //
+// Runtime:        554 ms, faster than 5.16%
+// Memory Usage:  50.8 MB, less than  87.43%
 const isPowerOfThree = (n) => {
+  let i = 0, pow = 0;
+  while (pow < n) {
+    pow = Math.pow(3, i++);
+    if (pow === n) return true;
+  }
+  return false;
 };
 // ************************************************************************************************************************ //
 
