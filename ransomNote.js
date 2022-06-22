a180a038f75211446fe726526b408f2b39e82d72
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-22 17:11:22 +0300

    problem #383 init

---
+++ ransomNote.js
@@ -0,0 +1,12 @@
+// *************************************************   383. Ransom Note   ************************************************* //
+// Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+// Each letter in magazine can only be used once in ransomNote.
+// https://leetcode.com/problems/ransom-note/
+//
+const canConstruct = (ransomNote, magazine) => {
+};
+// ************************************************************************************************************************ //
+
+console.log(canConstruct('a','b'));
+console.log(canConstruct('aa', 'ab'));
+console.log(canConstruct('aa', 'aab'));
