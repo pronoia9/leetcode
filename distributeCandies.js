b276aceac4a41f363ceed3436abaced3fd6ecacb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-23 22:08:54 +0300

    problem #575 (119ms  |  56.4 MB)

--- distributeCandies.js
+++ distributeCandies.js
@@ -3,12 +3,22 @@
 // The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
 // Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
 // https://leetcode.com/problems/distribute-candies/
-// 
-// Runtime:      3380 ms, faster than 5.55%   |   3568 ms, faster than 5.55%
-// Memory Usage: 51.8 MB, less than  98.89%   |   52.1 MB, less than  93.89%
+//
+// Runtime:       3380 ms, faster than 5.55%   |   3568 ms, faster than 5.55%
+// Memory Usage:  51.8 MB, less than  98.89%   |   52.1 MB, less than  93.89%
+{
+  const distributeCandies = (candyType) => {
+    const diet = candyType.length / 2;
+    const totalTypes = candyType.reduce((types, curr) => (!types.includes(curr) && types.push(curr), types), []).length;
+    return diet <= totalTypes ? diet : totalTypes;
+  };
+}
+//
+// Runtime:        228 ms, faster than 28.89%   |   166 ms,  faster than 60.83%   |   119 ms,  faster than 93.89%
+// Memory Usage:  56.2 MB, less than   66.39%   |   56.3 MB, less than   45.28%   |   56.4 MB, less than   36.11%
 const distributeCandies = (candyType) => {
   const diet = candyType.length / 2;
-  const totalTypes = candyType.reduce((types, curr) => (!types.includes(curr) && types.push(curr), types), []).length;
+  const totalTypes = candyType.reduce((types, curr) => types.add(curr), new Set()).size;
   return diet <= totalTypes ? diet : totalTypes;
 };
 // ************************************************************************************************************************ //
@@ -21,12 +31,12 @@ console.log(distributeCandies([6, 6, 6, 6]));
 // Input: candyType = [1,1,2,2,3,3]
 // Output: 3
 // Explanation: Alice can only eat 6 / 2 = 3 candies.Since there are only 3 types, she can eat one of each type.
-  
+
 // Example 2:
 // Input: candyType = [1,1,2,3]
 // Output: 2
 // Explanation: Alice can only eat 4 / 2 = 2 candies.Whether she eats types[1, 2], [1, 3], or[2, 3], she still can only eat 2 different types.
-  
+
 // Example 3:
 // Input: candyType = [6,6,6,6]
 // Output: 1
