c95d6ff1907ac11d34124f68d85d27f95ddaedb6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-25 14:37:40 -0500

    problem #557 (init)

---
+++ reverseWords.js
@@ -0,0 +1,18 @@
+// ****************************************   557. Reverse Words in a String III   **************************************** //
+// Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+// https://leetcode.com/problems/reverse-words-in-a-string-iii/
+//
+const reverseWords = (s) => {
+};
+// ************************************************************************************************************************ //
+
+console.log(reverseWords("Let's take LeetCode contest"));
+console.log(reverseWords('God Ding'));
+
+// Example 1:
+// Input: s = "Let's take LeetCode contest"
+// Output: "s'teL ekat edoCteeL tsetnoc"
+
+// Example 2:
+// Input: s = "God Ding"
+// Output: "doG gniD"
