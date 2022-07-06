3cd5def8e644dbf339a4637deb499a74ace26818
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-05 23:43:04 -0400

    problem #128 (init)

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -9,6 +9,9 @@ const longestConsecutive = (nums) => {
 
 console.log(longestConsecutive([100, 4, 200, 1, 3, 2]));
 console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]));
+console.log(longestConsecutive([]));
+console.log(longestConsecutive([0]));
+console.log(longestConsecutive([1, 2, 0, 1]));
 
 // Example 1:
 // Input: nums = [100,4,200,1,3,2]
