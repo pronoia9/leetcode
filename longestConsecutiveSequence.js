8326ee4f765d6cb527cff4ef2089db95a53145d6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-06 12:06:06 -0400

    problem #128 (124 ms	 |  58.1 MB)

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -3,7 +3,17 @@
 // You must write an algorithm that runs in O(n) time.
 // https://leetcode.com/problems/longest-consecutive-sequence/
 //
+// Runtime:        124 ms, faster than 86.68%   |    167 ms, faster than 67.27%   |    173 ms, faster than 64.86%
+// Memory Usage:  58.1 MB, less than   44.04%   |   58.7 MB, less than   32.80%   |   58.5 MB, less than   33.44%
 const longestConsecutive = (nums) => {
+  if (nums.length < 1) return 0;
+  let set = new Set(nums.sort((a, b) => a - b)), prev = null, count = [0];
+  set.forEach(num => {
+    if (num === prev + 1) count[count.length - 1]++;
+    else count.push(1);
+    prev = num;
+  });
+  return Math.max(...count);
 };
 // ************************************************************************************************************************ //
 
