cf6031284f96c24ba9be6c682fd703440ec18975
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-07-28 10:27:09 +0300

    problem #206 (init)

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -1,22 +1,38 @@
-// *******************************************    92. Reverse Linked List II   ******************************************** //
-// Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
-// https://leetcode.com/problems/reverse-linked-list-ii/
-// Definition for singly-linked list.
-// function ListNode(val, next) {
-//   this.val = (val === undefined ? 0 : val)
-//   this.next = (next === undefined ? null : next)
-// }
+// *********************************************   206. Reverse Linked List   ********************************************* //
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+// https://leetcode.com/problems/reverse-linked-list/
+function ListNode(val, next) { (this.val = val === undefined ? 0 : val), (this.next = next === undefined ? null : next); }
+const list = (arr) => {
+  let list = new ListNode(), curr = list;
+  for (let i = 0; i < arr.length; i++) {
+    curr.val = arr[i];
+    if (i !== arr.length - 1) (curr.next = new ListNode()), (curr = curr.next);
+  }
+  return list;
+};
 //
-const reverseBetween = (head, left, right) => {};
+const reverseList = (head) => {  
+};
 // ************************************************************************************************************************ //
 
-console.log(reverseBetween([1, 2, 3, 4, 5], 2, 4));
-console.log(reverseBetween([5], 1, 1));
+console.log(reverseList(list([1, 2, 3, 4, 5])));
+console.log(reverseList(list([1, 2])));
+console.log(reverseList(list([])));
 
 // Example 1:
-// Input: head = [1,2,3,4,5], left = 2, right = 4
-// Output: [1,4,3,2,5]
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
 
 // Example 2:
-// Input: head = [5], left = 1, right = 1
-// Output: [5]
+// Input: head = [1,2]
+// Output: [2,1]
+
+// Example 3:
+// Input: head = []
+// Output: []
+
+// Constraints:
+// The number of nodes in the list is the range [0, 5000].
+// -5000 <= Node.val <= 5000
+
+// Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
