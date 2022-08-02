5b2824dd16f82014497ca28b86ea904392541d29
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-02 17:56:38 +0300

    problem #242 (105 ms  |  49.1 MB)

--- validAnagram.js
+++ validAnagram.js
@@ -5,7 +5,29 @@
 //
 // Runtime:        137 ms, faster than 37.61%   |    105 ms, faster than 74.51%
 // Memory Usage:  48.1 MB, less than   24.96%   |   49.1 MB, less than   12.17%
-const isAnagram = (s, t) =>  s.split('').sort().join('') === t.split('').sort().join('');
+const isAnagram = (s, t) => s.split('').sort().join('') === t.split('').sort().join('');
+//
+// Runtime:       3302 ms, faster than  5.00%   |   3282 ms, faster than  5.00%
+// Memory Usage:  47.4 MB, less than   32.38%   |   47.5 MB, less than   31.77%
+{
+  const isAnagram = (s, t) => {
+    if (s.length !== t.length) return false;
+    t = t.split('');
+    for (let i = 0; i < s.length; i++) {
+      const idx = t.indexOf(s[i]);
+      if (idx === -1) return false;
+      else t.splice(idx, 1);
+    }
+    return true;
+  };
+}
+//
+// Runtime:        151 ms, faster than 25.62%   |    103 ms, faster than 76.52%
+// Memory Usage:  47.9 MB, less than   28.41%   |   49.5 MB, less than    8.41%
+{
+  const isAnagram = (s, t) =>
+    s.length !== t.length ? false : s.split('').sort().join('') === t.split('').sort().join('');
+}
 // ************************************************************************************************************************ //
 
 console.log(isAnagram('anagram', 'nagaram'));
@@ -17,7 +39,7 @@ console.log(isAnagram('rat', 'car'));
 
 // Example 2:
 // Input: s = "rat", t = "car"
-// Output: false 
+// Output: false
 
 // Constraints:
 // 1 <= s.length, t.length <= 5 * 104
