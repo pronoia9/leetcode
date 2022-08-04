7d92a8a97866460a2d5fdea0bb565a3aee6aa78c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-04 22:09:08 +0300

    problem #350 (71 ms  |  44.6 MB)

--- intersectionOfTwoArraysII.js
+++ intersectionOfTwoArraysII.js
@@ -1,13 +1,29 @@
 // ****************************************   350. Intersection of Two Arrays II   **************************************** //
 // Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 // https://leetcode.com/problems/intersection-of-two-arrays-ii/
-// 
+//
+// Runtime:        101 ms, faster than 47.31%   |     71 ms, faster than 90.91%
+// Memory Usage:  44.2 MB, less than   34.05%   |   44.6 MB, less than   22.57%
 const intersect = (nums1, nums2) => {
+  if (nums1.length < nums2.length) {
+    const temp = nums1; nums1 = nums2; nums2 = temp;
+  }
+
+  const map1 = new Map(), map2 = new Map(), common = [];
+  nums1.forEach((num) => map1.set(num, (map1.get(num) || 0) + 1));
+  nums2.forEach((num) => map2.set(num, (map2.get(num) || 0) + 1));
+
+  map2.forEach((key, num) => {
+    if (map1.has(num)) for (let i = 0; i < Math.min(map1.get(num), key); i++) { common.push(num); }
+  });
+
+  return common;
 };
 // ************************************************************************************************************************ //
 
-console.log(intersect([1,2,2,1], [2,2]));
-console.log(intersect([4,9,5], [9,4,9,8,4]));
+console.log(intersect([1, 2, 2, 1], [2, 2]));
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
+console.log(intersect([1, 2], [2, 1]));
 
 // Example 1:
 // Input: nums1 = [1,2,2,1], nums2 = [2,2]
