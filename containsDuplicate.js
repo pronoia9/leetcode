8bff0f41e529550be51c291945fefbe50c749af6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-05 17:08:07 +0300

    problem #217 (129 ms  |  50.8 MB)

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -2,7 +2,20 @@
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 // https://leetcode.com/problems/contains-duplicate/
 //
-var containsDuplicate = function (nums) {};
+// Runtime:     6824 ms, faster than  5.00%
+// Memory Usage:  48 MB, less than   97.91%
+{
+  const containsDuplicate = (nums) => {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums.lastIndexOf(nums[i]) !== i) return true;
+    }
+    return false;
+  };
+}
+//
+// Runtime:        129 ms, faster than 60.50%
+// Memory Usage:  50.8 MB, less than   63.20%
+const containsDuplicate = (nums) => nums.length !== new Set(nums).size;
 // ************************************************************************************************************************ //
 
 console.log(containsDuplicate([1, 2, 3, 1]));
