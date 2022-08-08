df3befb0c91ebeedc89742ca52b20f6654437819
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-08 13:42:53 +0300

    problem #171 (79 ms (86.38%) |  43.5 MB (62.16%))

--- excelSheetColumnNumber.js
+++ excelSheetColumnNumber.js
@@ -7,16 +7,52 @@
 // ...
 // Z -> 26
 // AA -> 27
-// AB -> 28 
+// AB -> 28
 // ...
 // https://leetcode.com/problems/excel-sheet-column-number/
 //
-const titleToNumber = (columnTitle) => {};
+// Runtime:        104 ms, faster than 54.26%
+// Memory Usage:  43.5 MB, less than   67.26%
+{
+  const titleToNumber = (columnTitle) => {
+    let total = 0;
+    for (let i = 0, j = columnTitle.length - 1; i < columnTitle.length; i++, j--) {
+      // total += Math.pow(26, columnTitle.length - 1 - i) * (columnTitle.charCodeAt(i) - 64);
+      total += Math.pow(26, j) * (columnTitle.charCodeAt(i) - 64);
+    }
+    return total;
+  };
+}
+//
+// Runtime:        118 ms, faster than 35.13%
+// Memory Usage:  43.5 MB, less than   62.16%
+{
+  const titleToNumber = (columnTitle) => {
+    let total = 0;
+    for (let i = 0, j = columnTitle.length - 1; i < columnTitle.length; i++, j--) {
+      total = total * 26 + (columnTitle.charCodeAt(i) - 64);
+    }
+    return total;
+  };
+}
+//
+// Runtime:         79 ms, faster than 86.38%
+// Memory Usage:  43.5 MB, less than  62.16%
+const titleToNumber = (columnTitle) => columnTitle.split('').reduce((total, char) => total * 26 + (char.charCodeAt(0) - 64), 0)
 // ************************************************************************************************************************ //
+
+// console.log('A'.charCodeAt(0) - 1);
+// console.log(total * 26);
+// console.log(Math.pow(26, (columnTitle.length - i - 1)));
+// console.log(columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1);
+// console.log(columnTitle.charCodeAt(i) - 64);
 
 console.log(titleToNumber('A'));
 console.log(titleToNumber('AB'));
 console.log(titleToNumber('ZY'));
+console.log(titleToNumber('AAA'));
+console.log(titleToNumber('ZAA'));
+console.log(titleToNumber('FXSHRXW'));
 
 // Example 1:
 // Input: columnTitle = "A"
