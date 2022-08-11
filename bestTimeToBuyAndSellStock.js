72c689d9e241fe61bab49df6a844a0523b484d33
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-11 23:58:00 +0300

    #121 (111 ms (56.87%) | 51.5 MB (78.69%))

--- bestTimeToBuyAndSellStock.js
+++ bestTimeToBuyAndSellStock.js
@@ -1,4 +1,4 @@
-// ***************************************   121. Best Time to Buy and Sell Sto******************************************** //
+// ***********************************   121. Best Time to Buy and Sell Sto**************************************** //
 // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
@@ -30,7 +30,7 @@ const maxProfit = (prices) => {
     return profit;
   }, 0);
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4]));
 console.log(maxProfit([7, 6, 4, 3, 1]));
