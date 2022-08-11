3e48c1f848e2111978df9c1abfe4e5f63ba72ac3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-11 23:56:58 +0300

    #2 (104 ms (89.81%) | 48 MB (22.08%))

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -1,10 +1,10 @@
-// ************************************************   2. Add Two Numbers   ************************************************ //
+// ********************************************   2. Add Two Numbers   ******************************************** //
 // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 // https://leetcode.com/problems/add-two-numbers/
 //
-// Runtime:       181 ms,   faster than   22.06%
-// Memory Usage:  47.9 MB,  less than     24.95%
+// Runtime:        181 ms,  faster than 22.06%
+// Memory Usage:  47.9 MB, less than    24.95%
 {
   let addTwoNumbers = function (l1, l2) {
     let arr1 = [],
@@ -67,8 +67,8 @@
   };
 }
 //
-// Runtime:       117 ms,   faster than   74.25%
-// Memory Usage:  48.6 MB,  less than     14.69%
+// Runtime:       117 ms,  faster than 74.25%
+// Memory Usage:  48.6 MB, less than   14.69%
 {
   let addTwoNumbers = function (l1, l2) {
     let arr1 = [],
@@ -99,8 +99,8 @@
   };
 }
 //
-// Runtime:       104 ms,   faster than   89.81%
-// Memory Usage:  48 MB,    less than     22.08%
+// Runtime:       104 ms, faster than 89.81%
+// Memory Usage:  48 MB,  less than   22.08%
 export default function addTwoNumbers(l1, l2) {
   let arr1 = [],
     arr2 = [];
@@ -124,4 +124,4 @@ export default function addTwoNumbers(l1, l2) {
   }
   return l3;
 }
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
