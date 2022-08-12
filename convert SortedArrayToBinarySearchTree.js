06db0b705c960f7078f6902e35bcd62eba9959dd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-12 20:37:19 +0300

    #108 (91 ms (79.18%) | 43.9 MB (97.77%))

--- convert SortedArrayToBinarySearchTree.js
+++ convert SortedArrayToBinarySearchTree.js
@@ -4,16 +4,52 @@
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 //
 // Definition for a binary tree node.
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
+class TreeNode {
+  constructor(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
 }
 // @param {number[]} nums
 // @return {TreeNode}
+// 
+// Runtime:         80 ms, faster than 91.90%   |    101 ms, faster than 67.19%   |    163 ms, faster than  5.09%
+// Memory Usage:  44.6 MB, less than   45.07%   |   44.3 MB, less than   75.44%   |   43.5 MB, less than   99.69%
+let sortedArrayToBST = (nums) => {
+  const helper = (left, right) => {
+    if (left > right) return null;
+
+    let middle = Math.round((left + right) / 2);
+    let root = new TreeNode(nums[middle]);
+
+    root.left = helper(left, middle - 1);
+    root.right = helper(middle + 1, right);
+
+    return root;
+  };
+
+  return helper(0, nums.length - 1);
+};
 //
-let sortedArrayToBST = (nums) => {};
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/discuss/35252/Intuitive-Javascript-Solution
+// Runtime:      115 ms, faster than 45.33%   |    104 ms, faster than 63.08%   |     91 ms, faster than 79.18%
+// Memory Usage:  45 MB, less than   10.70%   |   44.9 MB, less than   24.20%   |   43.9 MB, less than   97.77%
+let sortedArrayToBST = (nums, left = 0, right = nums.length - 1) => {
+  if (left > right) return null;
+
+  let middle = Math.floor((left + right) / 2);
+  let root = new TreeNode(nums[middle]);
+
+  root.left = sortedArrayToBST(nums, left, middle - 1);
+  root.right = sortedArrayToBST(nums, middle + 1, right);
+
+  return root;
+};
 // **************************************************************************************************************** //
+
+console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
+console.log(sortedArrayToBST([1, 3]));
 
 // Example 1:
 // https://assets.leetcode.com/uploads/2021/02/18/btree1.jpg
