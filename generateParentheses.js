06559d7b7ebc4bcef0fabc979781e4d7f3cf2a5c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-15 16:09:44 +0300

    #22 (init)

---
+++ generateParentheses.js
@@ -0,0 +1,20 @@
+// *****************************************   22. Generate Parentheses   ***************************************** //
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+// https://leetcode.com/problems/generate-parentheses/
+// 
+const generateParenthesis = (n) => {};
+// **************************************************************************************************************** //
+
+console.log(generateParenthesis(3));
+console.log(generateParenthesis(1));
+
+// Example 1:
+// Input: n = 3
+// Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+// Example 2:
+// Input: n = 1
+// Output: ["()"]
+
+// Constraints:
+// 1 <= n <= 8
