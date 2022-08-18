19613487a25786391ca977fff1c48f0ffccaec75
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-18 21:43:09 +0300

    #46 (91 ms (78.28%) | 44.8 MB (76.20%))

--- permutations.js
+++ permutations.js
@@ -2,7 +2,22 @@
 // Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 // https://leetcode.com/problems/permutations/
 //
-const permute = (nums) => {};
+// Backtracking
+// Runtime:         99 ms, faster than 68.55%   |     91 ms, faster than 78.28%
+// Memory Usage:  45.6 MB, less than   19.01%   |   44.8 MB, less than   76.20%
+const permute = (nums) => {
+  const result = [];
+
+  function backtrack(stack, nums) {
+    if (!nums.length) result.push(stack);
+    for (let i = 0; i < nums.length; i++) {
+      backtrack([...stack, nums[i]], [...nums.slice(0, i), ...nums.slice(i + 1)]);
+    }
+  }
+  backtrack([], nums);
+
+  return result;
+};
 // **************************************************************************************************************** //
 
 console.log(permute([1, 2, 3]));
@@ -25,3 +40,6 @@ console.log(permute([1]));
 // 1 <= nums.length <= 6
 // -10 <= nums[i] <= 10
 // All the integers of nums are unique.
+
+// https://www.youtube.com/watch?v=s7AvT7cGdSo
+// https://leetcode.com/problems/permutations/discuss/654378/JavaScript-Simple-Backtracking-Solution
