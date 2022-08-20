7b89aaf75d4c6b7428e72b45a3c2e2824d212e3a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-20 14:27:19 +0300

    #215 (170 ms (33.96%) | 49 MB (29.72%))

--- KthLargestElementInAnArray.js
+++ KthLargestElementInAnArray.js
@@ -4,11 +4,58 @@
 // You must solve it in O(n) time complexity.
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 //
-var findKthLargest = (nums, k) => {};
+//
+{
+  const findKthLargest = (nums, k) => nums.sort((a, b) => b - a)[k - 1];
+}
+//
+// Quicksort partition
+// Runtime:       195 ms, faster than 25.71%   |   180 ms, faster than 30.32%   |   170 ms, faster than 33.96%
+// Memory Usage:  48.8 MB, less than  31.52%   |    50 MB, less than   23.67%   |    49 MB, less than   29.72%
+const findKthLargest = (nums, k) => {
+  k = nums.length - k;
+  const quickSelect = (l, r) => {
+    let pivot = nums[r], p = l;
+    for (let i = l; i < r; i++) {
+      // if nums[i] is <= pivot, then swap at the p index
+      if (nums[i] <= pivot) {
+        const temp = nums[p];
+        nums[p] = nums[i];
+        nums[i] = temp;
+        // every time thers a swap, increment the p pointer and put it in the next position
+        p += 1;
+      }
+    }
+    // swap the pivot value with the p index
+    const temp = nums[p];
+    nums[p] = nums[r];
+    nums[r] = temp;
+
+    if (p > k) return quickSelect(l, p - 1);
+    if (p < k) return quickSelect(p + 1, r);
+    else return nums[p];
+  };
+  return quickSelect(0, nums.length - 1);
+}
+//
+// Runtime:        176 ms, faster than 31.68%
+// Memory Usage:  48.7 MB, less than   32.36%
+{ const findKthLargest = (nums, k) => quickSort(nums)[nums.length - k]; }
+//
+// const findKthLargest = (nums, k) => {
+//   const quickSort = (array) => {
+//     if (array.length <= 1)  return array;
+//     let pivot = array[0], left = [], right = [];
+//     for (var i = 1; i < array.length; i++)  array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+//     return quickSort(left).concat(pivot, quickSort(right));
+//   }
+//   return quickSort(nums)[nums.length - k];
+// };
 // **************************************************************************************************************** //
 
 console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
 console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
+console.log(findKthLargest([7, 6, 5, 4, 3, 2, 1], 2));
 
 // Example 1:
 // Input: nums = [3,2,1,5,6,4], k = 2
@@ -21,3 +68,39 @@ console.log(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4));
 // Constraints:
 // 1 <= k <= nums.length <= 105
 // -104 <= nums[i] <= 104
+
+const swap = (arr, left, right) => {
+  const temp = arr[left];
+  arr[left] = arr[right];
+  arr[right] = temp;
+};
+
+const partition = (arr, left, right) => {
+  let pivot = arr[Math.floor((right + left) / 2)], //middle element
+    i = left, //left pointer
+    j = right; //right pointer
+  while (i <= j) {
+    while (arr[i] < pivot) i++;
+    while (arr[j] > pivot) j--;
+    if (i <= j) {
+      //swap two elements
+      swap(i, j, arr);
+      i++;
+      j--;
+    }
+  }
+  return i;
+};
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+  let index;
+  if (arr.length > 1) {
+    //index returned from partition
+    index = partition(arr, left, right);
+    //more elements on the left side of the pivot
+    if (left < index - 1) quickSort(arr, left, index - 1);
+    //more elements on the right side of the pivot
+    if (index < right) quickSort(arr, index, right);
+  }
+  return arr;
+};
