92658a258593f1071db3fcbc2d0deb2c54740fa5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-22 20:34:57 +0300

    #17 (79 ms (65.32%) | 42.3 MB (33.33%))

--- letterCombinationsOfAPhoneNumber.js
+++ letterCombinationsOfAPhoneNumber.js
@@ -3,13 +3,28 @@
 // A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 // https://assets.leetcode.com/uploads/2022/03/15/1200px-telephone-keypad2svg.png
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-// 
-const letterCombinations = (digits) => {};
+//
+// Backtracking
+// Runtime:         96 ms, faster than 33.28%   |     79 ms, faster than 65.32%
+// Memory Usage:  42.2 MB, less than   45.79%   |   42.3 MB, less than   33.33%
+const letterCombinations = (digits) => {
+  if (!digits) return [];
+  const res = [],
+    map = new Map(Object.entries({ 2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4: ['g', 'h', 'i'], 5: ['j', 'k', 'l'], 6: ['m', 'n', 'o'], 7: ['p', 'q', 'r', 's'], 8: ['t', 'u', 'v'], 9: ['w', 'x', 'y', 'z'] }));
+
+  const backtrack = (idx = 0, str = '') => {
+    if (str.length === digits.length) res.push(str);
+    else map.get(digits[idx]).forEach((c) => backtrack(idx + 1, str + c));
+  };
+  backtrack();
+  return res;
+};
 // **************************************************************************************************************** //
 
 console.log(letterCombinations('23'));
 console.log(letterCombinations(''));
 console.log(letterCombinations('2'));
+console.log(letterCombinations('5'));
 
 // Example 1:
 // Input: digits = "23"
@@ -26,3 +41,5 @@ console.log(letterCombinations('2'));
 // Constraints:
 // 0 <= digits.length <= 4
 // digits[i] is a digit in the range ['2', '9'].
+
+// https://www.youtube.com/watch?v=0snEunUacZY
