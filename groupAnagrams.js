0012779e97fc47052e287f268a19ec998aaaeecd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-25 21:45:45 +0300

    #49 (190 ms (62.42%) | 51.2 MB (99.65%))

--- groupAnagrams.js
+++ groupAnagrams.js
@@ -3,7 +3,16 @@
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 // https://leetcode.com/problems/group-anagrams/
 //
-const groupAnagrams = (strs) => {};
+// Runtime:        209 ms, faster than 48.62%   |    190 ms, faster than 62.42%   |    181 ms, faster than 69.35%
+// Memory Usage:  52.4 MB, less than   90.52%   |   51.2 MB, less than   99.65%   |   52.7 MB, less than   82.71%
+const groupAnagrams = (strs) => {
+  const count = new Map();
+  for (let i = 0; i < strs.length; i++) {
+    const chars = strs[i].split('').sort().join('');
+    count.set(chars, count.get(chars) ? [...count.get(chars), strs[i]] : [strs[i]]);
+  }
+  return Array.from(count.values());
+};
 // **************************************************************************************************************** //
 
 console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
@@ -26,3 +35,5 @@ console.log(groupAnagrams(['a']));
 // 1 <= strs.length <= 104
 // 0 <= strs[i].length <= 100
 // strs[i] consists of lowercase English letters.
+
+// https://www.youtube.com/watch?v=vzdNOK2oB2E
