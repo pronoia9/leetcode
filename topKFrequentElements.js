f4b7222693ec1cae1e6a99d2b1ee333a9f269ad6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-26 19:48:34 +0300

    #347 (86 ms (90.50%) | 46 MB (48.11%))

--- topKFrequentElements.js
+++ topKFrequentElements.js
@@ -1,11 +1,25 @@
 // ***************************************   347. Top K Frequent Elements   *************************************** //
 // Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 // https://leetcode.com/problems/top-k-frequent-elements/
-const topKFrequent = (nums, k) => {};
+//
+// Runtime:         99 ms, faster than 77.43%   |   86 ms, faster than 90.50%
+// Memory Usage:  46.6 MB, less than   39.45%   |   46 MB, less than   48.11%
+const topKFrequent = (nums, k) => {
+  const map = new Map(), bucket = [], res = [];
+  // add each number to map (and their count)
+  for (let num of nums) { map.set(num, (map.get(num) || 0) + 1); }
+  // add values to bucket per count: bucket[number count]: Set(values...)
+  for (let [value, key] of map) { bucket[key] = (bucket[key] || new Set()).add(value); }
+  // add the highest count of values (at the end of bucket) to results array
+  for (let i = bucket.length - 1; i >= 0 && res.length < k; i--) if (bucket[i]) res.push(...bucket[i])
+  return res;
+};
 // **************************************************************************************************************** //
 
 console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
 console.log(topKFrequent([1], 1));
+console.log(topKFrequent([1, 2], 2));
+console.log(topKFrequent([4, 1, -1, 2, -1, 2, 3], 2));
 
 // Example 1:
 // Input: nums = [1,1,1,2,2,3], k = 2
