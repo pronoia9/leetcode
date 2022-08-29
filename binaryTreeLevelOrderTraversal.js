6a783f1fc88dac9ac11215b9f9d436d82ca88916
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-08-29 19:37:23 +0300

    #102 (58 ms (99.53%) | 44.1 MB (78.23%))

--- binaryTreeLevelOrderTraversal.js
+++ binaryTreeLevelOrderTraversal.js
@@ -12,7 +12,26 @@ function TreeNode(val, left, right) {
 // @param {TreeNode} root
 // @return {number[][]}
 //
-const levelOrder = (root) => {};
+// BFS
+// O(n) time  |  O(n) space
+//
+// Runtime:         93 ms, faster than 58.97%   |     58 ms, faster than 99.53%
+// Memory Usage:  44.2 MB, less than   68.75%   |   44.1 MB, less than   78.23%
+const levelOrder = (root) => {
+  const q = [root], res = [];
+  while (q.length) {
+    let qLen = q.length, level = [];
+    for (let i = 0; i < qLen; i++) {
+      node = q[0]; q.splice(0, 1);
+      if (node) {
+        level.push(node.val);
+        q.push(node.left, node.right);
+      }
+    }
+    level.length && res.push(level);
+  }
+  return res;
+};
 // **************************************************************************************************************** //
 
 console.log(levelOrder(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
@@ -35,3 +54,5 @@ console.log(levelOrder(new TreeNode()));
 // Constraints:
 // The number of nodes in the tree is in the range [0, 2000].
 // -1000 <= Node.val <= 1000
+
+// https://www.youtube.com/watch?v=6ZnyEApgFYg
