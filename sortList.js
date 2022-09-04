1bfccd73593dc268caca0bf49d4313f59f2f6eb1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-04 16:32:23 +0300

    #148 (303 ms (38.40%) | 68.2 MB (61.15%))

--- sortList.js
+++ sortList.js
@@ -7,15 +7,55 @@ function ListNode(val, next) {
   this.val = val === undefined ? 0 : val;
   this.next = next === undefined ? null : next;
 }
+const arr = (list) => {
+  const arr = [];
+  while (list) {
+    arr.push(list.val);
+    list = list.next;
+  }
+  return arr;
+};
 //
 /** @param {ListNode} head **/
 /** @return {ListNode} **/
-const sortList = (head) => {};
+// Recursive, Merge sort
+// O(n logn) time  |  O(logn) space
+// Runtime:       325 ms, faster than 27.70%   |    303 ms, faster than 38.40%   |    276 ms, faster than 54.77%
+// Memory Usage: 68.5 MB, less than   56.20%   |   68.2 MB, less than   61.15%   |   71.4 MB, less than   26.08%
+const sortList = (head) => {
+  // base case
+  if (!head || !head.next) return head;
+  // split the list into two halfs
+  let left = head, right = getMid(head);
+  [right.next, right] = [null, right.next];
+  left = sortList(left);
+  right = sortList(right);
+  // sort
+  return merge(left, right);
+};
+
+function getMid(head) {
+  let slow = head, fast = head.next;
+  while (fast && fast.next) { slow = slow.next; fast = fast.next.next; };
+  return slow;
+}
+
+function merge(list1, list2) {
+  let dummy = new ListNode(), curr = dummy;
+  while (list1 && list2) {
+    if (list1.val < list2.val) { curr.next = list1; list1 = list1.next; }
+    else { curr.next = list2; list2 = list2.next; }
+    curr = curr.next;
+  }
+  list1 && (curr.next = list1);
+  list2 && (curr.next = list2);
+  return dummy.next;
+}
 // **************************************************************************************************************** //
 
-console.log(sortList(new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3, null))))));
-console.log(sortList(new ListNode(-1, new ListNode(5, new ListNode(3, new ListNode(4, new ListNode(0, null)))))));
-console.log(sortList(new ListNode()));
+console.log(arr(sortList(new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3, null)))))));
+console.log(arr(sortList(new ListNode(-1, new ListNode(5, new ListNode(3, new ListNode(4, new ListNode(0, null))))))));
+console.log(arr(sortList(new ListNode())));
 
 // Example 1:
 // Input: head = [4,2,1,3]
