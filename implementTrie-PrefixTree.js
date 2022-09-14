f5590772eef2daeb53eb05aeb2eea6b1e7e36b5c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 17:31:10 +0300

    #208 (230 ms (84.70%) | 61.5 MB (66.75%))

--- implementTrie-PrefixTree.js
+++ implementTrie-PrefixTree.js
@@ -6,18 +6,50 @@
 // boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
 // boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
 // https://leetcode.com/problems/implement-trie-prefix-tree/
+//
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.endOfWord = false;
+  }
+}
 
+// Runtime:        418 ms, faster than 13.97%   |    463 ms, faster than 10.60%   |    230 ms, faster than 84.70%
+// Memory Usage:  69.5 MB, less than   26.60%   |   69.6 MB, less than   26.22%   |   61.5 MB, less than   66.75%
 class Trie {
-  constructor() {}
+  constructor() {
+    this.root = new TrieNode();
+  }
 
   /** @param {string} word **/ /** @return {void} **/
-  insert(word) {}
+  insert(word) {
+    let curr = this.root;
+    for (const c of word) {
+      if (!Object.keys(curr.children).includes(c)) curr.children[c] = new TrieNode();
+      curr = curr.children[c];
+    }
+    curr.endOfWord = true;
+  }
 
   /** @param {string} word **/ /** @return {boolean} **/
-  search(word) {}
+  search(word) {
+    let curr = this.root;
+    for (const c of word) {
+      if (!Object.keys(curr.children).includes(c)) return false;
+      curr = curr.children[c];
+    }
+    return curr.endOfWord;
+  }
 
   /** @param {string} prefix **/ /** @return {boolean} **/
-  startsWith(prefix) {}
+  startsWith(prefix) {
+    let curr = this.root;
+    for (const c of prefix) {
+      if (!Object.keys(curr.children).includes(c)) return false;
+      curr = curr.children[c];
+    }
+    return true;
+  }
 }
 
 /**
@@ -30,19 +62,19 @@ class Trie {
 // **************************************************************************************************************** //
 
 const trie = new Trie();
-console.log(trie);
+console.log(trie.root);
 console.log(trie.insert("apple"));
-console.log(trie);
+console.log(trie.root);
 console.log(trie.search('apple'));
-console.log(trie);
+console.log(trie.root);
 console.log(trie.search('app'));
-console.log(trie);
+console.log(trie.root);
 console.log(trie.startsWith('app'));
-console.log(trie);
+console.log(trie.root);
 console.log(trie.insert('app'));
-console.log(trie);
+console.log(trie.root);
 console.log(trie.search("app"));
-console.log(trie);
+console.log(trie.root);
 
 // Example 1:
 // Input
@@ -64,3 +96,7 @@ console.log(trie);
 // 1 <= word.length, prefix.length <= 2000
 // word and prefix consist only of lowercase English letters.
 // At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+
+const x = { a: {}, b: {}, c: {} }
+
+console.log(Object.keys(x).includes('a'))
