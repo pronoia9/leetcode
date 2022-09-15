3933e368c8b5f2a8615532f0b1ac14e9142f0ede
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-15 14:36:26 +0300

    #162 (init)

--- findPeakElement.js
+++ findPeakElement.js
@@ -6,24 +6,25 @@
 // https://leetcode.com/problems/find-peak-element/
 //
 /** @param {number[]} nums **/ /** @return {number} **/
+// O(log n) time
+//
 const findPeakElement = (nums) => {};
 // **************************************************************************************************************** //
 
-// console.log(());
+console.log(findPeakElement([1, 2, 3, 1]));
+console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4]));
 
 // Example 1:
-
 // Input: nums = [1,2,3,1]
 // Output: 2
 // Explanation: 3 is a peak element and your function should return the index number 2.
-// Example 2:
 
+// Example 2:
 // Input: nums = [1,2,1,3,5,6,4]
 // Output: 5
 // Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
 
 // Constraints:
-
 // 1 <= nums.length <= 1000
 // -231 <= nums[i] <= 231 - 1
 // nums[i] != nums[i + 1] for all valid i.
