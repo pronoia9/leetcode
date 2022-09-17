8e72ca94daf8ef3e2a8d7977313f889e34571816
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-17 19:26:13 +0300

    #207 (143 ms (31.58%) | 47.6 MB (52.63%))

--- courseSchedule.js
+++ courseSchedule.js
@@ -6,10 +6,73 @@
 ///
 /** @param {number} numCourses **/ /** @param {number[][]} prerequisites **/ /** @return {boolean} **/
 //
-const canFinish = (numCourses, prerequisites) => {};
+//// Breadth? first search BFS
+// Depth first search DFS
+// O(n + p) time
+{
+  const canFinish = (numCourses, prerequisites) => {
+    const preMap = new Map();
+    const visitSet = new Set(); // all courses along the  curr DFs path
+
+    // map each course to prepreq list
+    for (const [course, prereq] of prerequisites) {
+      if (!preMap.has(prereq)) preMap.set(prereq, []);
+      preMap.set(course, [...(preMap.get(course) || []), prereq]);
+    }
+
+    const dfs = (crs) => {
+      if (visitSet.has(crs)) return false;
+      if (!preMap.get(crs)) return true;
+
+      visitSet.add(crs);
+      preMap.get(crs).forEach((preq) => {
+        if (!dfs(preq)) return false;
+      });
+
+      visitSet.delete(crs);
+      preMap.set(crs, []);
+      return true;
+    };
+
+    for (let crs = 0; crs < numCourses; crs++) {
+      if (!dfs(crs)) return false;
+    }
+    return true;
+  };
+}
+//
+// Runtime:        145 ms, faster than 30.11%   |    178 ms, faster than 16.81%   |    143 ms, faster than 31.58%
+// Memory Usage:  49.7 MB, less than   21.79%   |   47.8 MB, less than   46.89%   |   47.6 MB, less than   52.63%
+const canFinish = (numCourses, prerequisites) => {
+  const preMap = new Map();
+   for (const [course, prereq] of prerequisites) {
+     if (!preMap.has(prereq)) preMap.set(prereq, []);
+     preMap.set(course, [...(preMap.get(course) || []), prereq]);
+   }
+  
+  const visitedState = { unknown: 0, checkingPreReqs: 1, preReqMet: 2, };
+  const visitedStateByCourse = Array.from({ length: numCourses }, () => visitedState.unknown);
+  
+  const dfs = (course) => {
+    const state = visitedStateByCourse[course];
+    if (state === visitedState.checkingPreReqs) return false;
+    else if (state === visitedState.preReqMet) return true;
+    else if (state === visitedState.unknown) {
+      visitedStateByCourse[course] = visitedState.checkingPreReqs;
+      const preReqs = preMap.get(course);
+      for (let i = 0; i < preReqs.length; i++) if (!dfs(preReqs[i])) return false; 
+      visitedStateByCourse[course] = visitedState.preReqMet;
+      return true;
+    }
+  };
+
+  for (let course = 0; course < numCourses; course++) if (!dfs(course)) return false;
+  return true;
+};
 // **************************************************************************************************************** //
 
-console.log(canFinish(2, [[1, 0]]));
+console.log(canFinish(5, [[0, 1], [0, 2], [1, 3], [1, 4], [3, 4]]));
+// console.log(canFinish(2, [[1, 0]]));
 console.log(canFinish(2, [[1, 0], [0, 1]]));
 
 // Example 1:
