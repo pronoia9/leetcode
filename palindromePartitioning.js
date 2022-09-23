e770c21d6e7a8a8988133f0d8de891d2889367e0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-23 19:23:37 +0300

    #131 (273 ms (91.88%) | 63.1 MB (91.88%))

--- palindromePartitioning.js
+++ palindromePartitioning.js
@@ -5,7 +5,28 @@
 //
 /** @param {string} s **/ /** @return {string[][]} **/
 //
-const partition = (s) => {};
+// Bruteforce, backtracking
+// Runtime:        544 ms, faster than 15.58%   |    273 ms, faster than 91.88% 
+// Memory Usage:  81.2 MB, less than   42.16%   |   63.1 MB, less than   91.88%
+const isPalindrome = (str) => str === str.split('').reverse().join('');
+const partition = (s) => {
+  const res = [], part = [];
+  const dfs = (i) => {
+    if (i >= s.length) {
+      res.push([...part]);
+      return;
+    }
+    for (let j = i; j < s.length; j++) {
+      if (isPalindrome(s.slice(i, j + 1))) {
+        part.push(s.slice(i, j + 1));
+        dfs(j + 1);
+        part.pop();
+      }
+    }
+  }
+  dfs(0);
+  return res;
+};
 // **************************************************************************************************************** //
 
 console.log(partition('aab'));
