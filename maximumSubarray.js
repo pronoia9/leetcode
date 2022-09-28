a2dd3ba0dfed1d81a90579df3ae062897f90bd2c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-28 18:25:41 +0300

    #53 (init)

--- maximumSubarray.js
+++ maximumSubarray.js
@@ -5,7 +5,7 @@
 //
 /** @param {number[]} nums **/ /** @return {number} **/
 //
-var maxSubArray = function (nums) {};
+const maxSubArray = (nums) => {};
 // **************************************************************************************************************** //
 
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
