e21aef87a2c1d1e8d15b3acd89d4bf141bd61c33
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-03 14:31:07 +0300

    #54 (61 ms (94.62%) | 41.2 MB (99.40%))

--- spiralMatrix.js
+++ spiralMatrix.js
@@ -4,7 +4,29 @@
 //
 /** @param {number[][]} matrix **/ /** @return {number[]} **/
 //
-const spiralOrder = (matrix) => {};
+// O(m*n) time, O(m*n) space
+// Runtime:         89 ms, faster than 54.33%   |     77 ms, faster than 72.25%   |     61 ms, faster than 94.62%
+// Memory Usage:  41.9 MB, less than   46.54%   |   41.9 MB, less than   46.54%   |   41.2 MB, less than   99.40%
+const spiralOrder = (matrix) => {
+  const res = [];
+  let left = 0, right = matrix[0].length, top = 0, bottom = matrix.length;
+  while (left < right && top < bottom) {
+    // get every val in the top row
+    for (let i = left; i < right; i++)  res.push(matrix[top][i]);
+    top++;
+    // get every val in the right col
+    for (let i = top; i < bottom; i++) res.push(matrix[i][right - 1]);
+    right--;
+    if (!(left < right && top < bottom)) break;
+    // get every val in the bottom row
+    for (let i = right - 1; i > left - 1; i--) res.push(matrix[bottom - 1][i]);
+    bottom--;
+    // get every val in the left col
+    for (let i = bottom - 1; i > top - 1; i--) res.push(matrix[i][left]);
+    left++;
+  }
+  return res;
+};
 // **************************************************************************************************************** //
 
 console.log(
