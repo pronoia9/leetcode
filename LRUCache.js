e1a695b1beecbcfe615680ff8614121081037a13
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-04 15:41:13 +0300

    #146 (667 ms (67.87%) | 95.2 MB (96.44%))

--- LRUCache.js
+++ LRUCache.js
@@ -9,16 +9,30 @@
 //
 /** @param {number} capacity **/
 //
+// Runtime:        675 ms, faster than 63.14%   |    667 ms, faster than 67.87%
+// Memory Usage:  96.3 MB, less than   78.39%   |   95.2 MB, less than   96.44%
 class LRUCache {
-  constructor(capacity) {}
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.cache = new Map();
+  }
 
   /** @param {number} key **/ /** @return {number} **/
-  get(key) {}
+  get(key) {
+    if (!this.cache.has(key)) return -1;
+    const value = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, value);
+    return value;
+  }
 
   /** @param {number} key **/ /** @param {number} value **/ /** @return {void} **/
-  put(key, value) {}
+  put(key, value) {
+    if (this.cache.has(key)) this.cache.delete(key);
+    this.cache.set(key, value);
+    if (this.cache.size > this.capacity) this.cache.delete(this.cache.keys().next().value);
+  }
 }
-
 // Your LRUCache object will be instantiated and called as such:
 // var obj = new LRUCache(capacity)
 // var param_1 = obj.get(key)
@@ -26,25 +40,25 @@ class LRUCache {
 // **************************************************************************************************************** //
 
 const lru = new LRUCache(2);
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.put(1, 1)); // cache is {1=1}
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.put(2, 2)); // cache is {1=1, 2=2}
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.get(1)); // return 1
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.put(3, 3)); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.get(2)); // returns -1 (not found)
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.put(4, 4)); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.get(1)); // return -1 (not found)
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.get(3)); // return 3
-console.log(lru);
+console.log(lru.cache);
 console.log(lru.get(4)); // return 4
-console.log(lru);
+console.log(lru.cache);
 
 // Example 1:
 // Input
