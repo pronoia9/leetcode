9107a3d4f524a5c6f4ec2a95c4a55a76de9a4036
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-07 14:14:01 +0300

    #55 (71 ms (98.34%) | 45.8 MB (93.49%))

--- jumpGame.js
+++ jumpGame.js
@@ -2,10 +2,18 @@
 // You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 // Return true if you can reach the last index, or false otherwise.
 // https://leetcode.com/problems/jump-game/
-// 
+//
 /** @param {number[]} nums **/ /** @return {boolean} **/
-// 
-const canJump = (nums) => {};
+//
+// Greedy
+// O(n) time, O(1) space
+// Runtime:        114 ms, faster than 68.92%   |     71 ms, faster than 98.34%
+// Memory Usage:  46.8 MB, less than   40.87%   |   45.8 MB, less than   93.49%
+const canJump = (nums) => {
+  let goal = nums[nums.length - 1];
+  for (let i = nums.length - 1; i >= 0; i--) if (i + nums[i] >= goal) goal = i;
+  return goal == 0;
+};
 // **************************************************************************************************************** //
 
 console.log(canJump([2, 3, 1, 1, 4]));
