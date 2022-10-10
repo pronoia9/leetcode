a571d5c491cfe9715aec99c36efa7ea719058732
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-10 17:36:31 +0300

    #50 (87 ms (69.63%) | 42.1 MB (49.43%))

--- pow(x,n).js
+++ pow(x,n).js
@@ -1,15 +1,28 @@
 // ***********************************************   50. Pow(x, n)   ********************************************** //
 // Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 // https://leetcode.com/problems/powx-n/
-// 
+//
 /** @param {number} x **/ /** @param {number} n **/ /** @return {number} **/
-// 
-const myPow = (x, n) => {};
+//
+// Runtime:        100 ms, faster than 47.20%   |    109 ms, faster than 29.48%   |     87 ms, faster than 69.63%
+// Memory Usage:  42.3 MB, less than   38.85%   |   42.2 MB, less than   49.43%   |   42.1 MB, less than   49.43%
+const myPow = (x, n) => {
+  if (n == 0) return 1;
+  if (x == 0) return 0;
+  if (n == 1) return x;
+  if (n == -1) return 1 / x;
+  if (n % 2) return x * myPow(x, n - 1);
+  else {
+    let res = myPow(x, n / 2);
+    return res * res;
+  }
+};
 // **************************************************************************************************************** //
 
-console.log(myPow(2.00000, 10));
-console.log(myPow(2.10000, 3));
-console.log(myPow(2.00000, -2));
+console.log(myPow(2.0, 10));
+console.log(myPow(2.1, 3));
+console.log(myPow(2.0, -2));
+console.log(myPow(34.00515, -3));
 
 // Example 1:
 // Input: x = 2.00000, n = 10
