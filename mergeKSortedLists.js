f5fea1e184a0128e3eceb8e56f82b72cf1ee624c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-17 18:11:26 +0300

    #23 (127 ms (85.10%) | 49 MB (25.96%))

--- mergeKSortedLists.js
+++ mergeKSortedLists.js
@@ -13,7 +13,42 @@ class ListNode {
 //
 /** @param {ListNode[]} lists **/ /** @return {ListNode} **/
 //
-const mergeKLists = (lists) => {};
+// Runtime:        795 ms, faster than 11.45%
+// Memory Usage:  47.3 MB, less than   73.91%
+{
+  const mergeKLists = (lists) => {
+    if (!lists || !lists.length) return null;
+    for (let i = 1; i < lists.length; i++) lists[0] = mergeLists(lists[0], lists[i]);
+    return lists[0];
+  };
+  function mergeLists(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    if (l1.val < l2.val) {
+      l1.next = mergeLists(l1.next, l2);
+      return l1;
+    } else {
+      l2.next = mergeLists(l1, l2.next);
+      return l2;
+    }
+  }
+}
+//
+// Runtime:       209 ms, faster than 46.86%   |   127 ms, faster than 85.10%
+// Memory Usage: 50.2 MB, less than   18.03%   |    49 MB, less than   25.96%
+const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
+const mergeKLists = (lists) => {
+  const queue = new MinPriorityQueue((a) => a.val); // { priority: (x) => x.val }
+  for (const head of lists) if (head) queue.enqueue(head);
+  let res = new ListNode(), curr = res;
+  while (!queue.isEmpty()) {
+    const { val, next } = queue.dequeue(); //.element;
+    curr.next = new ListNode(val);
+    curr = curr.next;
+    if (next) queue.enqueue(next);
+  }
+  return res.next;
+};
 // **************************************************************************************************************** //
 
 console.log(
@@ -23,9 +58,18 @@ console.log(
     new ListNode(2, new ListNode(6)),
   ])
 );
+console.log(
+  arrayify(
+    mergeKLists([
+      new ListNode(1, new ListNode(4, new ListNode(5))),
+      new ListNode(1, new ListNode(3, new ListNode(4))),
+      new ListNode(2, new ListNode(6)),
+    ])
+  )
+);
 console.log(mergeKLists([]));
 console.log(mergeKLists([[]]));
-console.log(mergeKLists([new ListNode(val, next)]));
+console.log(mergeKLists([new ListNode()]));
 
 // Example 1:
 // Input: lists = [[1,4,5],[1,3,4],[2,6]]
@@ -54,3 +98,12 @@ console.log(mergeKLists([new ListNode(val, next)]));
 // -104 <= lists[i][j] <= 104
 // lists[i] is sorted in ascending order.
 // The sum of lists[i].length will not exceed 104.
+
+function arrayify(list) {
+  const arr = [];
+  while (list) {
+    arr.push(list.val);
+    list = list.next;
+  }
+  return arr;
+}
