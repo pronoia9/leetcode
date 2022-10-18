5402c0abc659fdd7d22893870b8826fded350c50
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-18 14:13:43 +0300

    #239 (343 ms 98.06%) | 78.4 MB (88.67%))

--- slidingWindowMaximum.js
+++ slidingWindowMaximum.js
@@ -4,12 +4,27 @@
 // https://leetcode.com/problems/sliding-window-maximum/
 // 
 /** @param {number[]} nums **/ /** @param {number} k **/ /** @return {number[]} **/
-// 
-const maxSlidingWindow = (nums, k) => {};
+//
+// O(n) time
+// untime:         700 ms, faster than 68.27%   |   572 ms, faster than 80.55%   |    343 ms, faster than 98.06%
+// Memory Usage:  91.1 MB, less than    44.94   |    92 MB, less than   22.05%   |   78.4 MB, less than   88.67%
+const maxSlidingWindow = (nums, k) => {
+  const q = [], res = [];
+  for (let i = 0; i < nums.length; i++) {
+    while (q && nums[q[q.length - 1]] <= nums[i]) q.pop();
+    q.push(i);
+    // remove first element if its outside the window
+    if (q[0] === i - k) q.shift();
+    // if window has k elements add to results (first k-1 windows have a < k elements b/c we start from empty window and add 1 element each loop)
+    if (i >= k - 1) res.push(nums[q[0]]);
+  }
+  return res;
+};
 // **************************************************************************************************************** //
 
 console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3));
 console.log(maxSlidingWindow([1], 1));
+console.log(maxSlidingWindow([1, -1], 1));
 
 // Example 1:
 // Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
