d8bd71e91e83e52e7dbd4f57ba973e1ca1bd710d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-10-22 14:22:49 +0300

    #11 (88 ms (74.80%) | 49 MB (95.16%))

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -1,4 +1,4 @@
-// ******************************************   11. Container With Most Water   ******************************************* //
+// ***************************************   11. Container With Most Water   ************************************** //
 // You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 // Find two lines that together with the x-axis form a container, such that the container contains the most water.
 // Return the maximum amount of water a container can store.
@@ -24,8 +24,8 @@
   };
 }
 //
-// Runtime:      134 ms,  faster than 17.74%   |   119 ms,  faster than 31.78%   |   136 ms, faster than 16.37%   |   88 ms, faster than 74.80% 
-// Memory Usage: 49.6 MB, less than   51.97%   |   50.1 MB, less than   15.26%   |   50 MB,  less than   23.62%   |   49 MB, less than   95.16%
+// Runtime:      134 ms,  faster than 17.74%   |   88 ms, faster than 74.80% 
+// Memory Usage: 49.6 MB, less than   51.97%   |   49 MB, less than   95.16%
 const maxArea = (height) => {
   let area = 0;
   for (let left = 0, right = height.length - 1; left < right; height[left] > height[right] ? right-- : left++) {
@@ -54,9 +54,25 @@ const maxArea = (height) => {
 //     return prev < area ? area : prev;
 //   }, 0);
 // };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
+
 console.log(maxArea([2, 3, 4, 5, 18, 17, 6]));
-// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
-// console.log(maxArea([1, 1]));
-// console.log(maxArea([4, 3, 2, 1, 4]));
-// console.log(maxArea([2, 1]));
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxArea([1, 1]));
+console.log(maxArea([4, 3, 2, 1, 4]));
+console.log(maxArea([2, 1]));
+
+// Example 1:
+// https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/17/question_11.jpg
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+
+// Example 2:
+// Input: height = [1,1]
+// Output: 1
+
+// Constraints:
+// n == height.length
+// 2 <= n <= 105
+// 0 <= height[i] <= 104
