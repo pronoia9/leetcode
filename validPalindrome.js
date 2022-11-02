6472bc85eafb84dff9aec64306522ce554d40074
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-02 17:11:14 +0300

    #125 (70 ms (94.75%) | 45.9 MB (52.56%))

--- validPalindrome.js
+++ validPalindrome.js
@@ -1,4 +1,4 @@
-// **********************************************   125. Valid Palindrome   *********************************************** //
+// *******************************************   125. Valid Palindrome   ****************************************** //
 // A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 // Given a string s, return true if it is a palindrome, or false otherwise.
 // https://leetcode.com/problems/valid-palindrome/
@@ -9,7 +9,7 @@ const isPalindrome = (s) => {
   const str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
   return str === str.split('').reverse().join('');
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 // console.log(isPalindrome('A man, a plan, a canal: Panama'));
 // console.log(isPalindrome('race a car'));
