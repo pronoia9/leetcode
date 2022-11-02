22d61c3f342a7b27f32b6217607318df05d7788e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-02 17:16:04 +0300

    #709 (79 ms (58.47%) | 41.8 MB (81.90%))

--- toLowerCase.js
+++ toLowerCase.js
@@ -1,4 +1,4 @@
-// ************************************************   709. To Lower Case   ************************************************ //
+// ********************************************   709. To Lower Case   ******************************************** //
 // Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
 // https://leetcode.com/problems/to-lower-case/
 //
@@ -9,7 +9,7 @@
 // Runtime:         55 ms, faster than 98.26%   |   68 ms,   faster than 80.86%   |   79 ms,   faster than 58.47%
 // Memory Usage:  41.9 MB, less than   61.02%   |   42.3 MB, less than   28.54%   |   41.8 MB, less than   81.90%
 const toLowerCase = (s) => s.replace(/[A-Z]/g, (match) => match.toLowerCase());
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(toLowerCase('Hello'));
 console.log(toLowerCase('here'));
