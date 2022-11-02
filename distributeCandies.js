e89b04b374484477a5f9630c0bac5ebc6a4da86f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-02 16:10:32 +0300

    #575 (119 ms (93.89%) | 56.4 MB (36.11%))

--- distributeCandies.js
+++ distributeCandies.js
@@ -1,4 +1,4 @@
-// *********************************************   575. Distribute Candies   ********************************************** //
+// ******************************************   575. Distribute Candies   ***************************************** //
 // Alice has n candies, where the ith candy is of type candyType[i]. Alice noticed that she started to gain weight, so she visited a doctor.
 // The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
 // Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
@@ -27,7 +27,7 @@
 // Runtime:        248 ms, faster than 28.69%   |   177 ms,  faster than 59.50%   |   176 ms,  faster than 60.06%
 // Memory Usage:  56.4 MB, less than   36.61%   |   56.3 MB, less than   45.63%   |   56.3 MB, less than   55.31%
 const distributeCandies = (candyType) => Math.min(candyType.length / 2, new Set(candyType).size);
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(distributeCandies([1, 1, 2, 2, 3, 3]));
 console.log(distributeCandies([1, 1, 2, 3]));
