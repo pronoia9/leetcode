7620aa75b132261b4344453876095001d0105405
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-02 17:31:37 +0300

    #541 (77 ms (75.46%) | 45.1 MB (43.01%))

--- reverseStringII.js
+++ reverseStringII.js
@@ -1,4 +1,4 @@
-// **********************************************   541. Reverse String II   ********************************************** //
+// ******************************************   541. Reverse String II   ****************************************** //
 // Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
 // If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
 // https://leetcode.com/problems/reverse-string-ii/
@@ -27,7 +27,7 @@ const reverseStr = (s, k) => {
   }
   return arr.join('');
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(reverseStr('abcdefg', 2));
 console.log(reverseStr('abcd', 2));
