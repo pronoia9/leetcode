ced6b0fd046e2f865943d7a60d05075703023de1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-02 17:26:06 +0300

    #645 (83 ms (91.54%) | 49.7 MB (23.87%))

--- setMismatch.js
+++ setMismatch.js
@@ -1,4 +1,4 @@
-// ************************************************   645. Set Mismatch   ************************************************* //
+// ********************************************   645. Set Mismatch   ********************************************* //
 // You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
 // You are given an integer array nums representing the data status of this set after the error.
 // Find the number that occurs twice and the number that is missing and return them in the form of an array.
@@ -25,7 +25,7 @@ const findErrorNums = (nums) => {
   sum = nums.reduce((acc, num) => (set.has(num) ? (duplicate = num) : set.add(num), (acc += num)), 0);
   return [duplicate, total - (sum - duplicate)];
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(findErrorNums([1, 2, 2, 4]));
 console.log(findErrorNums([1, 1]));
