4fbc6d76c2cefa6807a3b7ae5e1206835d543bcc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-02 17:23:40 +0300

    #136 (1247 ms (5.01%) | 42.9 MB (92.22%))

--- singleNumber.js
+++ singleNumber.js
@@ -1,4 +1,4 @@
-// ************************************************   136. Single Number   ************************************************ //
+// ********************************************   136. Single Number   ******************************************** //
 // Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 // You must implement a solution with a linear runtime complexity and use only constant extra space.
 // https://leetcode.com/problems/single-number/
@@ -24,9 +24,7 @@
 {
   const singleNumber = (nums) => nums.find((num) => nums.indexOf(num) === nums.lastIndexOf(num));
 }
-//
-const singleNumber = (nums) => {}
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(singleNumber([2, 2, 1]));
 console.log(singleNumber([4, 1, 2, 1, 2]));
