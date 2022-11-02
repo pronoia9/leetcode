ead946f553d96dc6c2457722c9de144d904a908c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-02 17:55:31 +0300

    #344 (112 ms (78.20%) | 49.3 MB (66.06%))

--- reverseString.js
+++ reverseString.js
@@ -1,4 +1,4 @@
-// ************************************************   344. Reverse String   *********************************************** //
+// ********************************************   344. Reverse String   ******************************************* //
 // Write a function that reverses a string. The input string is given as an array of characters s.
 // You must do this by modifying the input array in-place with O(1) extra memory.z
 // https://leetcode.com/problems/reverse-string/
@@ -30,7 +30,7 @@ const reverseString = (s) => {
   }
   return s;
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(reverseString(['h', 'e', 'l', 'l', 'o']));
 console.log(reverseString(['H', 'a', 'n', 'n', 'a', 'h']));
