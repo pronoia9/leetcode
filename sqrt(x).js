4765e5c7d57d84c804d609895aa054a5e23111c2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-02 17:17:06 +0300

    #69 (78 ms (92.28%) | 43.8 MB (53.38%))

--- sqrt(x).js
+++ sqrt(x).js
@@ -1,4 +1,4 @@
-// ***************************************************   69. Sqrt(x)   **************************************************** //
+// ************************************************   69. Sqrt(x)   *********************************************** //
 // Given a non-negative integer x, compute and return the square root of x.
 // Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
 // Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
@@ -36,7 +36,7 @@ const mySqrt = (x) => Math.floor(Math.sqrt(x));
     return res;
   };
 }
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(mySqrt(4));
 console.log(mySqrt(8));
