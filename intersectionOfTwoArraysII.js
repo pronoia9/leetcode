e76a3dcaf0447e6bb05bd4f6c66f84ecca53dc6e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-05 00:53:54 +0300

    #350 (71 ms (90.91%) | 44.6 MB (22.57%))

--- intersectionOfTwoArraysII.js
+++ intersectionOfTwoArraysII.js
@@ -1,4 +1,4 @@
-// ****************************************   350. Intersection of Two Arrays II   **************************************** //
+// ************************************   350. Intersection of Two Arrays II   ************************************ //
 // Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 // https://leetcode.com/problems/intersection-of-two-arrays-ii/
 //
@@ -19,7 +19,7 @@ const intersect = (nums1, nums2) => {
 
   return common;
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(intersect([1, 2, 2, 1], [2, 2]));
 console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
