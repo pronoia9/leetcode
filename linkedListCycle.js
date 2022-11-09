0c5b2e137fe959fa55224df73cad9f5eaafefb3b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-09 14:22:12 +0300

    #141 (83 ms (86.26%) | 45.8 MB (14.52%))

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -1,4 +1,4 @@
-// **********************************************   141. Linked List Cycle   ********************************************** //
+// ******************************************   141. Linked List Cycle   ****************************************** //
 // Given head, the head of a linked list, determine if the linked list has a cycle in it.
 // There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 // Return true if there is a cycle in the linked list. Otherwise, return false.
@@ -34,7 +34,7 @@ const hasCycle = (head) => {
     return head !== null;
   };
 }
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 let cl1 = new ListNode(3, new ListNode(2, new ListNode(0, new ListNode(-4)))),
   cl2 = new ListNode(1, new ListNode(2)),
