bd56c5e4cafcc1370ce4b9bb60ec4ffad8ede9ef
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-20 21:49:27 +0300

    # (283 (83 ms (98.88%) | 45.9 MB (99.13%))

--- moveZeroes.js
+++ moveZeroes.js
@@ -1,4 +1,4 @@
-// *************************************************   283. Move Zeroes   ************************************************* //
+// *********************************************   283. Move Zeroes   ********************************************* //
 // Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 // Note that you must do this in-place without making a copy of the array.
 // https://leetcode.com/problems/move-zeroes/
@@ -12,7 +12,7 @@ const moveZeroes = (nums) => {
     return 0;
   })
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(moveZeroes([0, 1, 0, 3, 12]));
 console.log(moveZeroes([0]));
