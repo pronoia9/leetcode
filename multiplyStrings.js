e5aa76584b05d08ec852a45f4560d9de7689fc85
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-21 08:38:03 +0300

    # (81 ms (80.06%) | 44.9 MB (48.31%))

--- multiplyStrings.js
+++ multiplyStrings.js
@@ -1,4 +1,4 @@
-// ***********************************************   43. Multiply Strings   *********************************************** //
+// *******************************************   43. Multiply Strings   ******************************************* //
 // Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
 // Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
 // https://leetcode.com/problems/multiply-strings/
@@ -37,7 +37,7 @@ const multiply = (num1, num2) => {
 
   return result.join('').replace(/^0+/, '');
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(multiply('2', '3'));
 console.log(multiply('123', '456'));
