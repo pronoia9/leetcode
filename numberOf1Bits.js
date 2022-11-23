cbb9cad7ccd7a17b06e3479a597334050981ef0c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-23 10:34:38 +0300

    #191 (78 ms (70.17%) | 41.7 MB (99.55%))

--- numberOf1Bits.js
+++ numberOf1Bits.js
@@ -1,4 +1,4 @@
-// **********************************************   191. Number of 1 Bits   *********************************************** //
+// ******************************************   191. Number of 1 Bits   ******************************************* //
 // Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
 // Note:
 // Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
@@ -7,11 +7,9 @@
 // 
 // Runtime:      78 ms,   faster than 70.17%
 // Memory Usage: 41.7 MB, less than   99.55%
-const hammingWeight = (n) => { 
-  return n.toString(2).replace(/[^1]/g, '').length;
-};
-// ************************************************************************************************************************ //
+const hammingWeight = (n) => n.toString(2).replace(/[^1]/g, '').length;
+// **************************************************************************************************************** //
 
-// console.log(hammingWeight(00000000000000000000000000001011));
-// console.log(hammingWeight(00000000000000000000000010000000));
-// console.log(hammingWeight(11111111111111111111111111111101));
+console.log(hammingWeight(00000000000000000000000000001011));
+console.log(hammingWeight(00000000000000000000000010000000));
+console.log(hammingWeight(11111111111111111111111111111101));
