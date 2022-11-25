deb9a38eb6d77f48c5bc6f1f819a86ee7414ba29
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-25 11:25:29 +0300

    #118 (61 ms (93.37%) | 42.2 MB (36.37%))

--- pascalsTriangle.js
+++ pascalsTriangle.js
@@ -1,4 +1,4 @@
-// **********************************************   118. Pascal's Triangle   ********************************************** //
+// ******************************************   118. Pascal's Triangle   ****************************************** //
 // Given an integer numRows, return the first numRows of Pascal's triangle.
 // In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 // https://leetcode.com/problems/pascals-triangle/
@@ -16,7 +16,7 @@ const generate = (numRows) => {
   }
   return res;
 };
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 console.log(generate(5));
 console.log(generate(1));
