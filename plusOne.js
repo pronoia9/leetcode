b7558eeaae6d07ffcf4bd7a924c829863836aa43
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-11-26 14:30:15 +0300

    #66 (65 ms (81.52%) | 42.3 MB (32.87%))

--- plusOne.js
+++ plusOne.js
@@ -1,4 +1,4 @@
-// ***************************************************   66. Plus One   *************************************************** //
+// ***********************************************   66. Plus One   *********************************************** //
 // You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 // Increment the large integer by one and return the resulting array of digits.
 // https://leetcode.com/problems/plus-one/
@@ -44,8 +44,6 @@ export default function plusOne(digits) {
   };
 }
 //
-//
-//
 {
   const plusOne = (digits) => {
     let acc = BigInt(1);
@@ -58,8 +56,6 @@ export default function plusOne(digits) {
   };
 }
 //
-//
-//
 {
   const plusOne = (digits) => {
     let acc = BigInt(1);
@@ -71,7 +67,7 @@ export default function plusOne(digits) {
     return Array.from(`${acc}`, (n) => parseInt(n));
   };
 }
-// ************************************************************************************************************************ //
+// **************************************************************************************************************** //
 
 // console.log(plusOne([1, 2, 3]));
 // console.log(plusOne([4, 3, 2, 1]));
